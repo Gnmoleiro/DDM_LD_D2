bedab89c57b54aa34625908ad5002fd5522861a7
RpVitrine <92382045+RpVitrine@users.noreply.github.com>
2025-10-15 15:45:36 +0100

    design do login melhorado

--- ITask-app/src/app/pages/login/login.page.ts
+++ ITask-app/src/app/pages/login/login.page.ts
@@ -1,24 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonAlert, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent, 
-  IonList, IonItem, IonInput, IonInputPasswordToggle
+import { FormsModule, NgForm } from '@angular/forms';
+import {
+  IonContent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+  IonList, IonItem, IonInput, IonInputPasswordToggle, IonAlert
 } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonButton, IonCard, IonCardContent, IonList, IonItem, IonInput, IonInputPasswordToggle,
-    CommonModule, FormsModule]
+  imports: [
+    IonContent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+    IonList, IonItem, IonInput, IonInputPasswordToggle, CommonModule, FormsModule, RouterLink
+  ]
 })
 export class LoginPage implements OnInit {
 
-  alertButtons = ['Action'];
+  credentials = {
+    email: '',
+    password: ''
+  };
+
   constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  onLogin() {
+    const { email, password } = this.credentials;
+
+    if (!email || !password) {
+      console.log('Preencha todos os campos');
+      return;
+    }
+
+    console.log('Tentando login com:', email, password);
+  }
 }
