3e052c3109a1b79a9935c23f945560aa7654fd91
RpVitrine <92382045+RpVitrine@users.noreply.github.com>
2025-10-22 15:44:24 +0100

    bd atualizada

--- ITask-api/models.py
+++ ITask-api/models.py
@@ -13,51 +13,63 @@ class Departamento(enum.Enum):
     Marketing = "Marketing"
     Administracao = "Administração"
 
+class EstadoTarefa(enum.Enum):
+    ToDo = "ToDo"
+    Doing = "Doing"
+    Done = "Done"
+
 class User(db.Model):
     __tablename__ = 'users'
-    idUser = db.Column(db.String, primary_key=True, autoincrement=True)
+    idUser = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     nome = db.Column(db.String(100), nullable=False)
 
-class Programador(db.Model):
-    __tablename__ = 'programador'
-    idUser = db.Column(db.String, db.ForeignKey('users.idUser'), primary_key=True)
-    nivelExperiencia = db.Column(db.Enum(NivelExperiencia), default=NivelExperiencia.Junior, nullable=False)
-    user = db.relationship('User', backref=db.backref('programador', uselist=False))
-
 class Gestor(db.Model):
-    __tablename__ = 'gestor'
-    idUser = db.Column(db.String, db.ForeignKey('users.idUser'), primary_key=True)
+    __tablename__ = 'gestores'
+    idUser = db.Column(db.Integer, db.ForeignKey('users.idUser'), primary_key=True)
     departamento = db.Column(db.Enum(Departamento), default=Departamento.IT, nullable=False)
+    idDono = db.Column(db.Integer, db.ForeignKey('dono.idUser'), nullable=False)
     user = db.relationship('User', backref=db.backref('gestor', uselist=False))
+    dono = db.relationship('Dono', backref=db.backref('gestor', lazy=True))
 
-class Gere(db.Model):
-    __tablename__ = 'gere'
-    idProgramador = db.Column(db.String, db.ForeignKey('programador.idUser'), primary_key=True)
-    idGestor = db.Column(db.String, db.ForeignKey('gestor.idUser'), primary_key=True)
+class Programador(db.Model):
+    __tablename__ = 'programadores'
+    idUser = db.Column(db.Integer, db.ForeignKey('users.idUser'), primary_key=True)
+    idGestor = db.Column(db.Integer, db.ForeignKey('gestores.idUser'), nullable=False)
+    nivelExperiencia = db.Column(db.Enum(NivelExperiencia), default=NivelExperiencia.Junior, nullable=False)
+
+    user = db.relationship('User', backref=db.backref('programador', uselist=False))
+    gestor = db.relationship('Gestor', backref=db.backref('programadores', lazy=True))
+
+class Dono(db.Model):
+    __tablename__ = 'dono'
+    idUser = db.Column(db.Integer, db.ForeignKey('users.idUser'), primary_key=True)
+    empresa = db.Column(db.String, nullable=False)
 
 class TipoTarefa(db.Model):
-    __tablename__ = 'tipoTarefa'
-    idTipoTarefa = db.Column(db.String, primary_key=True, nullable=False)
+    __tablename__ = 'tipos_tarefa'
+    idTipoTarefa = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(100), nullable=False)
 
 class Tarefa(db.Model):
-    __tablename__ = 'tarefa'
-    idTarefa = db.Column(db.String, primary_key=True, autoincrement=True)
-    idGestor = db.Column(db.String, db.ForeignKey('gestor.idUser'), nullable=False)
-    idProgramador = db.Column(db.String, db.ForeignKey('programador.idUser'), nullable=False)
-    ordemExecucao = db.Column(db.Integer, nullable=False)
+    __tablename__ = 'tarefas'
+    idTarefa = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    idGestor = db.Column(db.Integer, db.ForeignKey('gestores.idUser'), nullable=False)
+    idProgramador = db.Column(db.Integer, db.ForeignKey('programadores.idUser'), nullable=False)
+    idTipoTarefa = db.Column(db.Integer, db.ForeignKey('tipos_tarefa.idTipoTarefa'), nullable=False)
+    
     descricao = db.Column(db.String(255), nullable=False)
+    ordemExecucao = db.Column(db.Integer, nullable=False)
+    storyPoint = db.Column(db.Integer, default=0, nullable=False)
+    estadoTarefa = db.Column(db.Enum(EstadoTarefa), default=EstadoTarefa.ToDo, nullable=False)
+    
     dataPrevistaInicio = db.Column(db.DateTime, nullable=False)
     dataPrevistaFim = db.Column(db.DateTime, nullable=False)
-    idTipoTarefa = db.Column(db.String, db.ForeignKey('tipoTarefa.idTipoTarefa'), nullable=False)
-    storyPoint = db.Column(db.Integer, nullable=True)
     dataRealInicio = db.Column(db.DateTime, nullable=True)
     dataRealFim = db.Column(db.DateTime, nullable=True)
     dataCriacao = db.Column(db.DateTime, default=datetime.utcnow)
-    estadoTarefa = db.Column(db.String(50), nullable=False)
 
-    gestor = db.relationship('Gestor', backref='tarefas')
-    programador = db.relationship('Programador', backref='tarefas')
-    tipoTarefa = db.relationship('TipoTarefa', backref='tarefas')
+    gestor = db.relationship('Gestor', backref=db.backref('tarefas', lazy=True))
+    programador = db.relationship('Programador', backref=db.backref('tarefas', lazy=True))
+    tipoTarefa = db.relationship('TipoTarefa', backref=db.backref('tarefas', lazy=True))
