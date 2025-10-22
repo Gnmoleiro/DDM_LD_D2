b742a9d754c7736e2554a963dfc4eeb302b481a6
RpVitrine <92382045+RpVitrine@users.noreply.github.com>
2025-10-22 15:47:54 +0100

    .

--- ITask-api/models.py
+++ ITask-api/models.py
@@ -20,23 +20,23 @@ class EstadoTarefa(enum.Enum):
 
 class User(db.Model):
     __tablename__ = 'users'
-    idUser = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    idUser = db.Column(db.String, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     nome = db.Column(db.String(100), nullable=False)
 
 class Gestor(db.Model):
     __tablename__ = 'gestores'
-    idUser = db.Column(db.Integer, db.ForeignKey('users.idUser'), primary_key=True)
+    idUser = db.Column(db.String, db.ForeignKey('users.idUser'), primary_key=True)
     departamento = db.Column(db.Enum(Departamento), default=Departamento.IT, nullable=False)
-    idDono = db.Column(db.Integer, db.ForeignKey('dono.idUser'), nullable=False)
+    idDono = db.Column(db.String, db.ForeignKey('dono.idUser'), nullable=False)
     user = db.relationship('User', backref=db.backref('gestor', uselist=False))
     dono = db.relationship('Dono', backref=db.backref('gestor', lazy=True))
 
 class Programador(db.Model):
     __tablename__ = 'programadores'
-    idUser = db.Column(db.Integer, db.ForeignKey('users.idUser'), primary_key=True)
-    idGestor = db.Column(db.Integer, db.ForeignKey('gestores.idUser'), nullable=False)
+    idUser = db.Column(db.String, db.ForeignKey('users.idUser'), primary_key=True)
+    idGestor = db.Column(db.String, db.ForeignKey('gestores.idUser'), nullable=False)
     nivelExperiencia = db.Column(db.Enum(NivelExperiencia), default=NivelExperiencia.Junior, nullable=False)
 
     user = db.relationship('User', backref=db.backref('programador', uselist=False))
@@ -44,20 +44,20 @@ class Programador(db.Model):
 
 class Dono(db.Model):
     __tablename__ = 'dono'
-    idUser = db.Column(db.Integer, db.ForeignKey('users.idUser'), primary_key=True)
+    idUser = db.Column(db.String, db.ForeignKey('users.idUser'), primary_key=True)
     empresa = db.Column(db.String, nullable=False)
 
 class TipoTarefa(db.Model):
     __tablename__ = 'tipos_tarefa'
-    idTipoTarefa = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    idTipoTarefa = db.Column(db.String, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
 
 class Tarefa(db.Model):
     __tablename__ = 'tarefas'
-    idTarefa = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    idGestor = db.Column(db.Integer, db.ForeignKey('gestores.idUser'), nullable=False)
-    idProgramador = db.Column(db.Integer, db.ForeignKey('programadores.idUser'), nullable=False)
-    idTipoTarefa = db.Column(db.Integer, db.ForeignKey('tipos_tarefa.idTipoTarefa'), nullable=False)
+    idTarefa = db.Column(db.String, primary_key=True)
+    idGestor = db.Column(db.String, db.ForeignKey('gestores.idUser'), nullable=False)
+    idProgramador = db.Column(db.String, db.ForeignKey('programadores.idUser'), nullable=False)
+    idTipoTarefa = db.Column(db.String, db.ForeignKey('tipos_tarefa.idTipoTarefa'), nullable=False)
     
     descricao = db.Column(db.String(255), nullable=False)
     ordemExecucao = db.Column(db.Integer, nullable=False)
